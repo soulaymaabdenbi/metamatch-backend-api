d4fcd1e5e32861af69528879656cf4188ec355b2
soulaymaabdenbi <soulayma.abdenbi@esprit.tn>
2024-02-24 17:22:52 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+
+
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('DB Connected'))
+    .catch(err => console.log(err));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/test', (req, res) => {
+    res.status(200).json({message: "Welcome to metamatch app"});
+
+});
+app.listen(process.env.PORT || 3000, () => console.log(`app listen on port ${process.env.PORT}!`));
