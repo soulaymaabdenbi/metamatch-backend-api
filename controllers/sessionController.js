c3330cdd279505a5842d40dc78e9a83c2c6150e0
Amina Mami <amina.mami@esprit.tn>
2024-03-15 02:09:11 +0100

    testing

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -14,6 +14,13 @@ const moment = require("moment");
 exports.addSession = async (req, res) => {
   const { date, time, location, topics } = req.body;
 
+  // Check if any required fields are missing
+  if (!date || !time || !location || !topics) {
+    return res
+      .status(400)
+      .json({ error: "Missing required fields: date, time, location, topics" });
+  }
+
   try {
     const existingSession = await Session.findOne({ date: date, time: time });
 
@@ -22,22 +29,18 @@ exports.addSession = async (req, res) => {
         .status(409)
         .json({ error: "Conflict: A session already exists at this time." });
     }
-  } catch (error) {
-    console.error("Error checking for conflicts:", error);
-    return res.status(500).json({ error: "Internal Server Error" });
-  }
 
-  try {
     const newSession = await Session.create({
       date,
       time,
       location,
       topics,
     });
-    res.status(201).json(newSession);
+
+    return res.status(201).json(newSession);
   } catch (error) {
     console.error("Error saving session:", error);
-    res.status(400).json({ error: error.message });
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
